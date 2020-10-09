cdd0da8e9f7e369818b91c3154a464cbeb30c042
greg <greg-incogneato@users.noreply.github.com>
2020-10-09 15:54:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,6 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.express as px
-import pandas as pd
-from dash.dependencies import Input, Output
-from urllib.request import urlopen
-import json
-=======
 import boto3
 from dash.dependencies import Input, Output
 from datetime import datetime
