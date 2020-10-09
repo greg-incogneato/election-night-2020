f1ee9dfc08e0019af574b173fd673eda9dd77c64
greg <greg-incogneato@users.noreply.github.com>
2020-10-09 15:51:24 -0400

    big change
    moving back-end data refresh to future AWS lambda script. app.py dedicated to refreshing data from static dash files to update charts

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-<<<<<<< Updated upstream
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
